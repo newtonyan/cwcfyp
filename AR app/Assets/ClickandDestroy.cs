53f874bbf197b5718e5cd8d20210bcc6a8220d25
yuhokkan <yuhokkan1@gmail.com>
2018-09-28 00:47:15 +0800

    Click and delete

---
+++ AR app/Assets/ClickandDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickandDestroy : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                BoxCollider bc = hit.collider as BoxCollider;
+                if (bc != null)
+                {
+                    Destroy(bc.gameObject);
+                }
+            }
+        }
+
+    }
+}
