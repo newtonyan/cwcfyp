2af59c8fce5d514675d831137d316885f8e5ed20
newtonyan <35562580+newtonyan@users.noreply.github.com>
2018-10-03 22:03:30 +0800

    Added GUI and Character

---
+++ AR app/Assets/Models/Supercyan Character Pack Free Sample/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private int xp = 0;
+    [SerializeField] private int requiredXp = 100;
+    [SerializeField] private int levelBase = 100;
+    [SerializeField] private List<GameObject> droids = new List<GameObject>();
+    private int lvl = 1;
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
