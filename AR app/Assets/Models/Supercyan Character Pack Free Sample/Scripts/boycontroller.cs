2f56f49f353622dfa287189feaa5ae54a0cb1438
yuhokkan <yuhokkan1@gmail.com>
2018-10-17 01:47:56 +0800

    people can move

--- AR app/Assets/Models/Supercyan Character Pack Free Sample/Scripts/boycontroller.cs
+++ AR app/Assets/Models/Supercyan Character Pack Free Sample/Scripts/boycontroller.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class boycontroller : MonoBehaviour {
 
+    static Animator anim;
+    public float speed = 2.0f;
+    public float rotationSpeed = 75.0f; 
+
 	// Use this for initialization
 	void Start () {
+
+        anim = GetComponent<Animator>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if (translation != 0)
+        {
+            anim.SetBool("IsWalking", true);
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+        }
+
+    }
 }
