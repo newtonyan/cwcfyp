3fa838e32df5c9fd2c8150403ddcb36146f31076
newtonyan <35562580+newtonyan@users.noreply.github.com>
2018-10-26 04:48:03 +0800

    Merge AR with game
    Solved Nav Mesh, center the world with the target

--- AR app/Assets/moverandomly.cs
+++ AR app/Assets/moverandomly.cs
@@ -43,7 +43,7 @@ public class moverandomly : MonoBehaviour {
 
         while (!validPath)
         {
-            yield return new WaitForSeconds(0.0001f);
+            yield return new WaitForSeconds(0.01f);
             GetNewPath();
             validPath = navMeshAgent.CalculatePath(target, path);
         }
