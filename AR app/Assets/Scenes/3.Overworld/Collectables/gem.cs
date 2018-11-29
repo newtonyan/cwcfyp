41b955952b045f87e1fde14a7cf546f4fd835c67
newtonyan <35562580+newtonyan@users.noreply.github.com>
2018-11-30 01:01:14 +0800

    Created gem prefab

---
+++ AR app/Assets/Scenes/3.Overworld/Collectables/gem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gem : MonoBehaviour {
+
+    [SerializeField] private int bonus = 10;
+
+    private void OnMouseDown()
+    {
+        CUHKGameManager.Instance.CurrentPlayer.AddCredit(bonus);
+        Destroy(gameObject);
+    }
+}
